b7b75445b1d4fd8846f56014b54d6c846c2f836e
Kaiva-Morphin <bebraeveryday0@gmail.com>
2023-12-01 20:49:21 +0300

    pre release version

--- scripts/Pathfinder.cs
+++ scripts/Pathfinder.cs
@@ -1,19 +1,17 @@
 using Godot;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
 
 
 public class WeightedSequence
 {
 	private class Node
 	{
-		public Spatial Data { get; }
+		public String Data { get; }
 		public float Weight { get; }
 		public Node Next { get; set; }
 
-		public Node(Spatial data, float weight)
+		public Node(String data, float weight)
 		{
 			Data = data;
 			Weight = weight;
@@ -23,7 +21,7 @@ public class WeightedSequence
 
 	private Node head;
 
-	public void Add_Element(Spatial data, float weight)
+	public void Add_Element(String data, float weight)
 	{
 		Node newNode = new Node(data, weight);
 
@@ -46,9 +44,9 @@ public class WeightedSequence
 		}
 	}
 
-	public Spatial Pop()
+	public String Pop()
 	{
-		Spatial result = head.Data;
+		String result = head.Data;
 		head = head.Next;
 		return result;
 	}
@@ -59,95 +57,110 @@ public class WeightedSequence
 	}
 }
 
+public class GraphData{
+	public Dictionary<String, List<String>> graph;
+	public Dictionary<List<String>, float> distances; // List<String> used like (String, String) because godot cant parse Tuple to json(((
 
-public class DataPack : Godot.Object{
-	public Dictionary<Spatial, HashSet<Spatial>> graph;
-	public Dictionary<(Spatial, Spatial), float> distances;
+	public Dictionary<String, List<String>> node_to_groups;
+	public Dictionary<String, List<String>> group_to_node;
 
-	public Dictionary<Spatial, HashSet<string>> node_to_groups;
-	public Dictionary<string, HashSet<Spatial>> group_no_node;
-	public string ToJson()
+
+
+	
+	public String Prepare()
 	{
-		return JSON.Print(this);
+		return JSON.Print(graph) + "\n" + JSON.Print(distances) + "\n" + JSON.Print(node_to_groups) + "\n" + JSON.Print(group_to_node);
 	}
-	public static DataPack FromJson(string json)
+	public static GraphData Parse(String data)
 	{
-		return JSON.Parse(json).Result as DataPack;
+		var parsed = data.Split();
+		return new GraphData{
+			graph = JSON.Parse(parsed[0]).Result as Dictionary<String, List<String>>,
+			distances = JSON.Parse(parsed[1]).Result as Dictionary<List<String>, float>,
+			node_to_groups = JSON.Parse(parsed[2]).Result as Dictionary<String, List<String>>,
+			group_to_node = JSON.Parse(parsed[3]).Result as Dictionary<String, List<String>>,
+		};
 	}
 }
 
 public class Pathfinder : Node
 {
+	// todo: change List -> HashSet (for optimization, but HashSet can't be converted to JSON unsing JSON.Print...)
+	private Dictionary<String, List<String>> graph;
+	private Dictionary<List<String>, float> distances;// List<String> used like (String, String) because godot cant parse Tuple to json :sad_emoji:
+
+	private Dictionary<String, List<String>> node_to_groups;
+	private Dictionary<String, List<String>> group_to_node;
 	
-	private Dictionary<Spatial, HashSet<Spatial>> graph;
-	private Dictionary<(Spatial, Spatial), float> distances;
+    public override void _Ready()
+    {
+        graph = new Dictionary<String, List<String>> {};
+		distances = new Dictionary<List<String>, float> {};
+		node_to_groups = new Dictionary<String, List<String>> {};
+		group_to_node = new Dictionary<String, List<String>> {};
+    }
 
-	private Dictionary<Spatial, HashSet<string>> node_to_groups;
-	private Dictionary<string, HashSet<Spatial>> group_no_node;
-
-	public void Save(string filename){
+	public void Save(){
 		using (var file = new File())
 		{
-			file.Open(filename, File.ModeFlags.Write);
-			var data = new DataPack
-			{
+            var data = new GraphData{
 				graph = graph,
 				distances = distances,
 				node_to_groups = node_to_groups,
-				group_no_node = group_no_node
+				group_to_node = group_to_node,
 			};
-			file.StoreVar(data);
+			file.Open("SAVE.save", File.ModeFlags.Write);
+			file.StoreString(data.Prepare());
 			file.Close();
-		}
-	}
-	public void Load(string filename){
-		graph.Clear();
-		distances.Clear();
-		node_to_groups.Clear();
-		group_no_node.Clear();
-		using (var file = new File())
-		{
-			file.Open(filename, File.ModeFlags.Read);
-			
-			var data = (DataPack)file.GetVar();
-			file.Close();
-			GD.Print(file.GetVar());
-			
-			/*graph = data.graph;
-			distances = data.distances;
-			node_to_groups = data.node_to_groups;
-			group_no_node = data.group_no_node;*/
-			
-		}
-	}
-	public override void _Ready()
-	{
-		graph = new Dictionary<Spatial, HashSet<Spatial>> {};
-		distances = new Dictionary<(Spatial, Spatial), float> {};
-		node_to_groups = new Dictionary<Spatial, HashSet<string>> {};
-		group_no_node = new Dictionary<string, HashSet<Spatial>> {};
-	}
-	
-	public void Set_group(Spatial node, string name){
-		if (node_to_groups.Keys.Contains(node)){
-			node_to_groups[node].Add(name);
-		} else {
-			node_to_groups[node] = new HashSet<string> {name};
-		}
-		if (group_no_node.Keys.Contains(name)){
-			group_no_node[name].Add(node);
-		} else {
-			group_no_node[name] = new HashSet<Spatial> {node};
 		}
 	}
 
-	public void Clear_groups(Spatial node){
-		if (node_to_groups.Keys.Contains(node)){
+	public void Load(){
+		graph = new Dictionary<String, List<String>> {};
+		distances = new Dictionary<List<String>, float> {};
+		node_to_groups = new Dictionary<String, List<String>> {};
+		group_to_node = new Dictionary<String, List<String>> {};
+		using (var file = new File())
+		{
+			file.Open("SAVE.save", File.ModeFlags.Read);
+			
+			var data = GraphData.Parse(file.ToString());
+			file.Close();
+			//var recovered = DataPack2.FromJson(data);
+			//GD.Print(recovered);
+			
+			graph = data.graph;
+			distances = data.distances;
+			node_to_groups = data.node_to_groups;
+			group_to_node = data.group_to_node;
+			
+		}
+	}
+
+	public void Set_group(String node, String name){ // todo: check. may be incorrect
+		if (node_to_groups.ContainsKey(node)){
+			if (!node_to_groups[node].Contains(name)){
+				node_to_groups[node].Add(name);
+			}
+		} else {
+			node_to_groups[node] = new List<String> {name};
+		}
+		if (group_to_node.ContainsKey(name)){
+			if (!group_to_node[name].Contains(node)){
+				group_to_node[name].Add(node);
+			}
+		} else {
+			group_to_node[name] = new List<String> {node};
+		}
+	}
+
+	public void Clear_groups(String node){ // todo: check. may be incorrect
+		if (node_to_groups.ContainsKey(node)){
 			var groups = node_to_groups[node];
 			foreach (var group in groups){
-				if (group_no_node.Keys.Contains(group)){
-					if (group_no_node[group].Contains(node)){
-						group_no_node[group].Remove(node);
+				if (group_to_node.ContainsKey(group)){
+					if (group_to_node[group].Contains(node)){
+						group_to_node[group].Remove(node);
 					}
 				}
 			}
@@ -155,151 +168,107 @@ public class Pathfinder : Node
 		}
 	}
 
-	public void Remove_group(Spatial node, string name){
-		if (node_to_groups.Keys.Contains(node)){
+	public void Remove_group(String node, String name){ // todo: check. may be incorrect
+		if (node_to_groups.ContainsKey(node)){
 			node_to_groups[node].Remove(name);
 		}
-		if (group_no_node.Keys.Contains(name)){
-			group_no_node[name].Remove(node);
+		if (group_to_node.ContainsKey(name)){
+			group_to_node[name].Remove(node);
 		}
 	}
 
-	public bool In_group(Spatial node, string name){
-		if (node_to_groups.Keys.Contains(node)){
+	public bool In_group(String node, String name){ // todo: check. may be incorrect
+		if (node_to_groups.ContainsKey(node)){
 			return node_to_groups[node].Contains(name);
 		}
 		return false;
 	}
 
-	public void Register_bind(Spatial start, Spatial end, float distance){
-		if (graph.Keys.Contains(start)){
+	public void Register_bind(String start, String end, float distance){ // todo: check. may be incorrect
+		if (graph.ContainsKey(start)){
 			if (!graph[start].Contains(end)){
 				graph[start].Add(end);
 			}
 		} else {
-			graph.Add(start, new HashSet<Spatial> {end});
+			graph.Add(start, new List<String> {end});
 		}
-		if (!distances.Keys.Contains((start, end))){
-			distances.Add((start, end), distance);
+		if (!distances.ContainsKey(new List<String>{start, end})){
+			distances.Add(new List<String>{start, end}, distance);
 		}
 		
 
-		if (graph.Keys.Contains(end)){
+		if (graph.ContainsKey(end)){
 			if (!graph[end].Contains(start)){
 				graph[end].Add(start);
 			}
 		} else {
-			graph.Add(end, new HashSet<Spatial> {start});
+			graph.Add(end, new List<String> {start});
 		}
-		if (!distances.Keys.Contains((end, start))){
-			distances.Add((end, start), distance);
+		if (!distances.ContainsKey(new List<String>{end, start})){
+			distances.Add(new List<String>{end, start}, distance);
 		}
 	}
 
-	public void Erase_point(Spatial point){
-		if (graph.Keys.Contains(point)){
+	public void Erase_point(String point){
+		if (graph.ContainsKey(point)){
 			foreach (var end in graph[point]){
 				if (graph[end].Contains(point)){
 					graph[end].Remove(point);
 				}
-				if (distances.Keys.Contains((point, end))){
-					distances.Remove((point, end));
+				if (distances.ContainsKey(new List<String>{point, end})){
+					distances.Remove(new List<String>{point, end});
 				}
-				if (distances.Keys.Contains((end, point))){
-					distances.Remove((end, point));
+				if (distances.ContainsKey(new List<String>{end, point})){
+					distances.Remove(new List<String>{end, point});
 				}
 			}
 			graph.Remove(point);
 		}
 	}
 
-	public void Break_bind(Spatial start, Spatial end){
-		if (graph.Keys.Contains(start)){
+	public void Break_bind(String start, String end){ // todo: check. may be incorrect
+		if (graph.ContainsKey(start)){
 			if (graph[start].Contains(end)){
 				graph[start].Remove(end);
 			}
 		}
-		if (graph.Keys.Contains(end)){
+		if (graph.ContainsKey(end)){
 			if (graph[end].Contains(start)){
 				graph[end].Remove(start);
 			}
 		}
-		if (distances.Keys.Contains((start, end))){
-			distances.Remove((start, end));
+		if (distances.ContainsKey(new List<String>{start, end})){
+			distances.Remove(new List<String>{start, end});
 		}
-		if (distances.Keys.Contains((end, start))){
-			distances.Remove((end, start));
+		if (distances.ContainsKey(new List<String>{end, start})){
+			distances.Remove(new List<String>{end, start});
 		}
 	}
-	public List<Spatial> Shortest_path(Spatial startVertex, Spatial endVertex)
+	public List<String> Shortest_path(String startVertex, String endVertex) // todo: check. may be incorrect
 	{
-		if (!(graph.Keys.Contains(startVertex) && graph.Keys.Contains(endVertex))){
-			return new List<Spatial> {};
+		if (!(graph.ContainsKey(startVertex) && graph.ContainsKey(endVertex))){
+			return new List<String> {};
 		}
+		GD.Print(startVertex);
+		GD.Print(endVertex);
 		WeightedSequence sequence = new WeightedSequence();
-		Dictionary<Spatial, Spatial> parent = new Dictionary<Spatial, Spatial>();
-		Dictionary<Spatial, float> visited = new Dictionary<Spatial, float>();
+		Dictionary<String, String> parent = new Dictionary<String, String>();
+		Dictionary<String, float> visited = new Dictionary<String, float>();
 
 		sequence.Add_Element(startVertex, 0);
 		visited[startVertex] = 0;
-
+		GD.Print("SEQ");
+		
 		while (!sequence.IsEmpty())
 		{
-			Spatial currentVertex = sequence.Pop();
-			
-			foreach (var neighbor in graph[currentVertex])
-			{
-				float dist = visited[currentVertex] + distances[(currentVertex, neighbor)];
-				if (!visited.ContainsKey(neighbor))
-				{
-					sequence.Add_Element(neighbor, dist);
-					visited[neighbor] = dist;
-					parent[neighbor] = currentVertex;
-				}
-				/*else {
-					if (visited[neighbor] > dist){
-						visited[neighbor] = dist;
-					}
-				}*/
+			String currentVertex = sequence.Pop();
+			if (currentVertex == endVertex){
+					endVertex = currentVertex;
+					break;
 			}
-		}
-
-		// Reconstruct the path
-		List<Spatial> shortestPath = new List<Spatial>();
-		Spatial tempVertex = endVertex;
-		if (!parent.Keys.Contains(tempVertex)){
-			return new List<Spatial> {};
-		}
-		while (tempVertex != startVertex)
-		{
-			shortestPath.Add(tempVertex);
-			tempVertex = parent[tempVertex];
-		}
-
-		shortestPath.Add(startVertex);
-		shortestPath.Reverse();
-
-		return shortestPath;
-	}
-	
-	public List<Spatial> Shortest_path_group(Spatial startVertex, string endGroup)
-	{
-		if (!graph.Keys.Contains(startVertex)){
-			return new List<Spatial> {};
-		}
-		WeightedSequence sequence = new WeightedSequence();
-		Dictionary<Spatial, Spatial> parent = new Dictionary<Spatial, Spatial>();
-		Dictionary<Spatial, float> visited = new Dictionary<Spatial, float>();
-
-		sequence.Add_Element(startVertex, 0);
-		visited[startVertex] = 0;
-		Spatial endVertex = null;
-		while (!sequence.IsEmpty())
-		{
-			Spatial currentVertex = sequence.Pop();
 			foreach (var neighbor in graph[currentVertex])
 			{
-				float dist = visited[currentVertex] + distances[(currentVertex, neighbor)];
+				float dist = visited[currentVertex] + distances[new List<String>{currentVertex, neighbor}];
 				if (!visited.ContainsKey(neighbor))
 				{
 					sequence.Add_Element(neighbor, dist);
@@ -309,22 +278,74 @@ public class Pathfinder : Node
 				else {
 					if (visited[neighbor] > dist){
 						visited[neighbor] = dist;
+						parent[neighbor] = currentVertex;
 					}
-				}
-				if (In_group(neighbor, endGroup)){
-					endVertex = neighbor;
-					break;
 				}
 			}
 		}
-		if (endVertex == null){
-			return new List<Spatial> {};
+
+		// Reconstruct the path
+		List<String> shortestPath = new List<String>();
+		String tempVertex = endVertex;
+		if (!parent.ContainsKey(tempVertex)){
+			return new List<String> {};
+		}
+		while (tempVertex != startVertex)
+		{
+			shortestPath.Add(tempVertex);
+			tempVertex = parent[tempVertex];
+		}
+
+		shortestPath.Add(startVertex);
+		shortestPath.Reverse();
+
+		return shortestPath;
+	}
+	
+	public List<String> Shortest_path_group(String startVertex, String endGroup) // todo: check. may be incorrect
+	{
+		if (!graph.ContainsKey(startVertex)){
+			return new List<String> {};
+		}
+		WeightedSequence sequence = new WeightedSequence();
+		Dictionary<String, String> parent = new Dictionary<String, String>();
+		Dictionary<String, float> visited = new Dictionary<String, float>();
+
+		sequence.Add_Element(startVertex, 0);
+		visited[startVertex] = 0;
+		String endVertex = null;
+		while (!sequence.IsEmpty())
+		{
+			String currentVertex = sequence.Pop();
+			if (In_group(currentVertex, endGroup)){
+					endVertex = currentVertex;
+					break;
+			}
+			foreach (var neighbor in graph[currentVertex])
+			{
+				float dist = visited[currentVertex] + distances[new List<String>{currentVertex, neighbor}];
+				if (!visited.ContainsKey(neighbor))
+				{
+					sequence.Add_Element(neighbor, dist);
+					visited[neighbor] = dist;
+					parent[neighbor] = currentVertex;
+				}
+				else {
+					if (visited[neighbor] > dist){
+						visited[neighbor] = dist;
+						parent[neighbor] = currentVertex;
+					}
+				}
+			}
+		}
+		if (endVertex == null){ // ?
+			return new List<String> {};
 		}
 		// Reconstruct the path
-		List<Spatial> shortestPath = new List<Spatial>();
-		Spatial tempVertex = endVertex;
-		if (!parent.Keys.Contains(tempVertex)){
-			return new List<Spatial> {};
+		List<String> shortestPath = new List<String>();
+		String tempVertex = endVertex;
+		if (!parent.ContainsKey(tempVertex)){ // ?
+			return new List<String> {};
 		}
 		while (tempVertex != startVertex)
 		{
@@ -338,3 +359,5 @@ public class Pathfinder : Node
 		return shortestPath;
 	}
 }
+
+
